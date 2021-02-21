ae18c68a98a925c68998a908c6c12350242b3182
lucas.mcazelli <lucas.mcazelli@gmail.com>
2021-02-21 10:39:14 +0000

    base

---
+++ script.js
@@ -0,0 +1,43 @@
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 8,
+      center: { lat: 63.333, lng: -150.5 },
+      mapTypeId: "terrain",
+    });
+    const elevator = new google.maps.ElevationService();
+    const infowindow = new google.maps.InfoWindow({});
+    infowindow.open(map);
+    // Add a listener for the click event. Display the elevation for the LatLng of
+    // the click inside the infowindow.
+    map.addListener("click", (event) => {
+      displayLocationElevation(event.latLng, elevator, infowindow);
+    });
+  }
+  
+  function displayLocationElevation(location, elevator, infowindow) {
+    // Initiate the location request
+    elevator.getElevationForLocations(
+      {
+        locations: [location],
+      },
+      (results, status) => {
+        infowindow.setPosition(location);
+  
+        if (status === "OK" && results) {
+          // Retrieve the first result
+          if (results[0]) {
+            // Open the infowindow indicating the elevation at the clicked position.
+            infowindow.setContent(
+              "The elevation at this point <br>is " +
+                results[0].elevation +
+                " meters."
+            );
+          } else {
+            infowindow.setContent("No results found");
+          }
+        } else {
+          infowindow.setContent("Elevation service failed due to: " + status);
+        }
+      }
+    );
+  }
